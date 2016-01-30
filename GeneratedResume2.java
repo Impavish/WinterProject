e4ddab5da0224bdef70012ebe18d1cfd37d6a2a5
Impavish <torres.eduardo.steven@gmail.com>
2016-01-30 07:03:37 -0600

    Reupload

---
+++ GeneratedResume2.java
@@ -0,0 +1,23 @@
+import java.util.Calendar;
+import java.sql;
+
+//This portion of my script would be reliant on data provided from my SQL database, however, here I have just set it up using variables to show how it would look.
+
+public class GeneratedResume {
+	int hoursworked = 42;
+	boolean org = false;
+	string orgname = Bellevue East ROTC;
+	string location = Omaha;
+
+		public static void main (String[] args) {
+			System.out.print("You've volunteered a total of" + hourswokred "hours, in the" + location "area!"\n);
+				if (org == true){
+				//This segment adds a line about a user being part of an organization when they volunteered.
+					System.out.print("These hours were completed while a part of " + orgname)
+					}
+				}
+			}
+			
+			
+	
+
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# WinterProject
-# WinterProject
-# WinterProject
+// Welcome to my Gallup Winter Project. Contained in this Git Repository are a few essential compoents of my project.
+// The first thing I include, in the SQLSetter.java file, is the tables that the program will create whenever various activities are performed.
+// Next, I included another portion of my script that provides a sample of what the program's auto-generated resumes could look like.
+// Please, feel free to direct any questions to torres.eduardo.steven@gmail.com
